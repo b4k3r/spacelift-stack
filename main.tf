5483e7db99cdeccc387c43156bdd560c55db7975
Marcin Prokop <marcin@prokop.co>
2022-06-29 12:07:55 +0200

    Simple terraform

---
+++ main.tf
@@ -0,0 +1,24 @@
+provider "spacelift" {}
+
+terraform {
+  required_providers {
+    spacelift = {
+      source = "spacelift-io/spacelift"
+    }
+  }
+}
+
+resource "spacelift_context" "foo" {
+  description = "Nice description"
+  name        = "foo context"
+}
+resource "spacelift_context" "bar" {
+  description = "Awesome description"
+  name        = "bar context"
+}
+
+# resource "spacelift_policy" "no-weekend-deploys" {
+#   name = "Let's not deploy any changes over the weekend"
+#   body = "package rego \n deny { falase }"
+#   type = "TERRAFORM_PLAN"
+# }
